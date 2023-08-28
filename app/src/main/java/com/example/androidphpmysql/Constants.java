b68896f491819e9da4fc117ae7ebaa6df945a5ee
coder <soumyodipsutra@gmail.com>
2023-08-29 00:05:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/androidphpmysql/Constants.java
@@ -0,0 +1,8 @@
+package com.example.androidphpmysql;
+
+public class Constants {
+
+    public static final String ROOT_URL="https//192.168.0.161/Android/v1/";
+
+    public static final String URL_REGISTER=ROOT_URL+"registerUser.php";
+}
---
+++ app/src/main/java/com/example/androidphpmysql/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.androidphpmysql;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextUserName,editTextPassword,editTextEmail;
+    private Button buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextEmail=findViewById(R.id.editTextEmail);
+        editTextPassword=findViewById(R.id.editTextPassword);
+        editTextUserName=findViewById(R.id.editTextUserName);
+        buttonRegister=findViewById(R.id.buttonRegister);
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        final String email = editTextEmail.getText().toString().trim();
+        final String username = editTextUserName.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(getApplicationContext(),jsonObject.getString("message"),Toast.LENGTH_LONG).show();
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username",username);
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+            }
+        };
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+    }
+    @Override
+    public void onClick(View view) {
+        if(view==buttonRegister)
+            registerUser();
+    }
+
+}
